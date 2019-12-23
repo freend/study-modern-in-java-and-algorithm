e82820ec8ea48ebd956ff2807f7cd0bfd9b96f74
freend <freending@gmail.com>
2019-12-23 16:39:42 +0900

    Test : Delete Method
    Delete unused method

--- src/test/java/com/freend/algorithm/programers/Test20191218Tests.java
+++ src/test/java/com/freend/algorithm/programers/Test20191218Tests.java
@@ -96,48 +96,6 @@ public class Test20191218Tests {
         }
         return answer;
     }
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        int sum = 0;
-        int i = 0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        int[] car_d = new int[truck_weights.length];
-
-        while(true) {
-            answer++;
-            //while문이므로 i를 직접 설정해서 새로운 트럭이 다리를 지나가면 증가시켜준다.
-            if(i<truck_weights.length && sum + truck_weights[i] <= weight) {
-                q.add(truck_weights[i]);
-                sum += truck_weights[i];
-
-                i++;
-            }
-            // -1은 다리를 이미 건넜다는 표시이다.
-            for(int j=0; j<i; j++) {
-                if(car_d[j] != -1) {
-                    car_d[j]++;
-                }
-            }
-            //시간이 길이와 같으면 나오면서 전체 무게를 빼주고 시간 배열을 -1로 바꿔준다.
-            for(int j=0; j<i; j++) {
-                if(car_d[j] == bridge_length) {
-                    int complete = q.poll();
-                    arr.add(complete);
-                    sum -= complete;
-                    car_d[j] = -1;
-                }
-            }
-            //마지막에는 시간 추가가 안되므로 나오면서 answer++을 해준다.
-            if(arr.size() == truck_weights.length) {
-                answer++;
-                break;
-            }
-
-        }
-
-        return answer;
-    }
     /**
      * 프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
      *
@@ -196,16 +154,5 @@ public class Test20191218Tests {
         }
 
         return answer;
-    }
-    public int[] completeProcessGood(int[] progresses, int[] speeds) {
-        int[] dayOfend = new int[100];
-        int day = -1;
-        for(int i = 0; i < progresses.length; i++) {
-            while(progresses[i] + (day * speeds[i]) < 100) {
-                day++;
-            }
-            dayOfend[day]++;
-        }
-        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
