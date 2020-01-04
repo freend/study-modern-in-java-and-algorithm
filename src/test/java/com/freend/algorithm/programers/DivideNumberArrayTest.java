78f1785484bb43d2491583029c989b83fddea565
freend <freending@gmail.com>
2020-01-04 19:38:00 +0900

    Test DivideNumberArrayTest array item check for to Arrays.toString()

--- src/test/java/com/freend/algorithm/programers/DivideNumberArrayTest.java
+++ src/test/java/com/freend/algorithm/programers/DivideNumberArrayTest.java
@@ -3,6 +3,8 @@ package com.freend.algorithm.programers;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class DivideNumberArrayTest {
     DivideNumberArray divideNumberArray;
     @Before
@@ -15,22 +17,18 @@ public class DivideNumberArrayTest {
         int[] arr = {5, 9, 7, 10};
         int divisor = 5;
         int[] answer = this.divideNumberArray.solution(arr, divisor);
-        for (int i = 0; i < answer.length; i++) {
-            System.out.println(answer[i]);
-        }
+        System.out.println(Arrays.toString(answer));
+
         System.out.println("---------------------");
         arr = new int[]{2, 36, 1, 3};
         divisor = 1;
         answer = this.divideNumberArray.solution(arr, divisor);
-        for (int i = 0; i < answer.length; i++) {
-            System.out.println(answer[i]);
-        }
+        System.out.println(Arrays.toString(answer));
+
         System.out.println("---------------------");
         arr = new int[]{3, 2, 6};
         divisor = 10;
         answer = this.divideNumberArray.solution(arr, divisor);
-        for (int i = 0; i < answer.length; i++) {
-            System.out.println(answer[i]);
-        }
+        System.out.println(Arrays.toString(answer));
     }
 }
