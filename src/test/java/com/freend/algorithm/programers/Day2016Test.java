3de3e025524bc4c0e2cc0efadcaf778ec04f6926
freend <freending@gmail.com>
2020-01-06 21:21:02 +0900

    Test : Add Day2016Test
    Add Day2016Test.java

---
+++ src/test/java/com/freend/algorithm/programers/Day2016Test.java
@@ -0,0 +1,20 @@
+package com.freend.algorithm.programers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Day2016Test {
+    Day2016 day2016;
+    @Before
+    public void setUp() {
+        this.day2016 = new Day2016();
+    }
+
+    @Test
+    public void 오늘은_무슨요일() {
+        assertThat(this.day2016.solution(5, 24), equalTo("TUE"));
+    }
+}
