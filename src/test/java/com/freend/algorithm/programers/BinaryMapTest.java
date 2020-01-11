771f5fe37edf29e5571a11915dbfad32f38f8ae8
freend <freending@gmail.com>
2020-01-11 17:48:06 +0900

    Test : add test
    add BinaryMapTest.java

---
+++ src/test/java/com/freend/algorithm/programers/BinaryMapTest.java
@@ -0,0 +1,29 @@
+package com.freend.algorithm.programers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BinaryMapTest {
+    BinaryMap binaryMap;
+    @Before
+    public void setUp() {
+        this.binaryMap = new BinaryMap();
+    }
+    @Test
+    public void binaryMap() {
+        int n = 5;
+        int[] arr1 = new int[] {9, 20, 28, 18, 11};
+        int[] arr2 = new int[] {30, 1, 21, 17, 28};
+        assertThat(this.binaryMap.binaryMap(n, arr1, arr2), equalTo(new String[]{"#####", "# # #", "### #", "#  ##", "#####"}));
+    }
+    @Test
+    public void sample() {
+        int n = 6;
+        int[] arr1 = new int[] {46, 33, 33 ,22, 31, 50};
+        int[] arr2 = new int[] {27 ,56, 19, 14, 14, 10};
+        assertThat(this.binaryMap.binaryMap(n, arr1, arr2), equalTo(new String[]{"######", "###  #", "##  ##", " #### ", " #####", "### # "}));
+    }
+}
