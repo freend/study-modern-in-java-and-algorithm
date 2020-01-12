0ecfdbc1abe9b8de052d79019c7b9d2c65eb9cc7
freend <freending@gmail.com>
2020-01-12 19:38:52 +0900

    Test : Add Test
    Add FailRateTest.java

---
+++ src/test/java/com/freend/algorithm/programers/FailRateTest.java
@@ -0,0 +1,24 @@
+package com.freend.algorithm.programers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FailRateTest {
+    FailRate failRate;
+    @Before
+    public void setUp() {
+        this.failRate = new FailRate();
+    }
+    @Test
+    public void 실패율_정렬_배열() {
+        int n = 5;
+        int[] stage = new int[] {2, 1, 2, 6, 2, 4, 3, 3};
+        assertThat(this.failRate.failRateList(n, stage), equalTo(new int[] {3,4,2,1,5}));
+        n = 4;
+        stage = new int[] {4,4,4,4,4};
+        assertThat(this.failRate.failRateList(n, stage), equalTo(new int[] {4,1,2,3}));
+    }
+}
