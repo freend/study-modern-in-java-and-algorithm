acaa1e3fc328e9636d329096da84e003c86e5caf
freend <freending@gmail.com>
2020-01-14 12:44:35 +0900

    Test : add test
    add StrangeLettersTest.java

---
+++ src/test/java/com/freend/algorithm/programers/StrangeLettersTest.java
@@ -0,0 +1,20 @@
+package com.freend.algorithm.programers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StrangeLettersTest {
+    StrangeLetters strangeLetters;
+    @Before
+    public void setUp() {
+        this.strangeLetters = new StrangeLetters();
+    }
+    @Test
+    public void 이상한_문자열_만들기() {
+        String string = "  try   hello world";
+        assertThat(this.strangeLetters.solution(string), equalTo("  TrY   HeLlO WoRlD"));
+    }
+}
