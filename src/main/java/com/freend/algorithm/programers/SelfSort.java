f0521189436818a634f6c4824aef4d7c4279fc54
Freend Choi <freending@gmail.com>
2020-01-15 12:08:13 +0900

    Feat : Add Algorithm
    Add SelfSort.java

---
+++ src/main/java/com/freend/algorithm/programers/SelfSort.java
@@ -0,0 +1,41 @@
+package com.freend.algorithm.programers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SelfSort {
+    public String[] SelfSort(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        Arrays.sort(strings);
+        List<SortItem> list = new ArrayList<>();
+        for (String value : strings) {
+            list.add(new SortItem(String.valueOf(value.charAt(n)), value));
+        }
+
+        list.sort(Comparator.comparing(SortItem::getSortString));
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).getTotalString();
+        }
+        return answer;
+    }
+    static class SortItem {
+        private String sortString;
+        private String totalString;
+
+        public SortItem(String sortString, String totalString) {
+            this.sortString = sortString;
+            this.totalString = totalString;
+        }
+
+        public String getSortString() {
+            return sortString;
+        }
+
+        public String getTotalString() {
+            return totalString;
+        }
+    }
+}
