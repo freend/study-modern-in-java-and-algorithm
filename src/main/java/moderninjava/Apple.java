8d69a69d06eab022e2b6c295838f04762671a6e0
Freend Choi <freending@gmail.com>
2020-01-18 20:45:48 +0900

    Feat : Add Algorithm
    Add HateSameNumber.java

---
+++ src/main/java/moderninjava/Apple.java
@@ -0,0 +1,15 @@
+package moderninjava;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Apple {
+    private String color;
+    private Double weight;
+}
---
+++ src/main/java/moderninjava/Predicate.java
@@ -0,0 +1,5 @@
+package moderninjava;
+
+public interface Predicate<T> {
+    boolean isFind(T t);
+}
